dad0155e9d2a5cab3e1307b4f6357acd2a11a322
gbritt <gbritt91@gmail.com>
2016-12-13 18:25:43 -0500

    test

--- bot/slack_clients.py
+++ bot/slack_clients.py
@@ -2,6 +2,9 @@
 import logging
 import re
 import time
+import json
+import argparse
+import os
 
 from slacker import Slacker
 from slackclient import SlackClient
@@ -18,6 +21,8 @@ class SlackClients(object):
 
         # SlackClient is a Slack Websocket RTM API Client
         self.rtm = SlackClient(token)
+
+
 
     def bot_user_id(self):
         return self.rtm.server.login_data['self']['id']
@@ -36,3 +41,43 @@ class SlackClients(object):
         user_typing_json = {"type": "typing", "channel": channel_id}
         self.rtm.server.send_to_websocket(user_typing_json)
         time.sleep(sleep_time)
+    def upload_file(self,file,channel_id):
+        #self.rtm.api_call('')
+        self.rtm.api_call('files.upload', channel = channel_id)
+        #this is not working
+
+
+    def get_chat_history(self,channel_id, pageSize = 100):
+        '''
+        channels = slack.channels.list().body['channels']
+        messages = []
+        lastTimestamp = None
+        response = channel.history(
+        channel = channel_id
+        latest = lastTimestamp
+        oldest = 0
+        count = pageSize
+        ).body
+        messages.extend(response['messages'])
+        return messages
+        '''
+        #history = self.rtm.api_call('channels.history', channel = channel_id)
+        channelHistory = self.rtm.api_call('channels.history', channel = channel_id)
+        return channelHistory
+
+        '''
+        with open(channelHistory, 'r') as handle:
+            parsed = json.load(handle)
+        '''
+
+
+        '''
+        with open(fileName, 'w') as outFile:
+            print("writing {0} records to {1}".format(len(messages), fileName))
+            json.dump({'channel_info': channelInfo, 'messages': messages }, outFile, indent=4)
+
+        channels = slack.channels.list().body['channels']
+        return (channel['name'])
+        #print self.rtm.api_call("chat.post_Message", as_user = "true:", #channel = channel_id, text = test)
+        #self.trm.server.send_to_websocket
+        '''
